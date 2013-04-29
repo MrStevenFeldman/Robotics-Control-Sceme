e9ea30c791e20c1f5c2edec55ab935f367dbc4c2
Steven Feldman <Mr.StevenFeldman@gmail.com>
2013-04-28 23:57:48 -0400

    Initial Commit

---
+++ DalekController/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DalekController
+//
+//  Created by Steven Feldman on 4/28/13.
+//  Copyright (c) 2013 Steven FELDMAN. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
